ca4031013462b3a356fe7236f5079e668c983e4c
Mynul Islam <maynul.programmer24@gmail.com>
2023-12-10 22:19:44 +0600

    Slider code add

--- index.php
+++ index.php
@@ -15,5 +15,13 @@
     <?php wp_footer(); ?>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, modi obcaecati facilis nesciunt tempora deserunt, odit sint qui, sunt corrupti perspiciatis tenetur quas consequuntur officiis voluptates nostrum exercitationem molestiae mollitia? </p>
+    <div class="slider-container">
+  <div class="slider">
+    <div class="slide"><img src="image1.jpg" alt="Slide 1"></div>
+    <div class="slide"><img src="image2.jpg" alt="Slide 2"></div>
+    <div class="slide"><img src="image3.jpg" alt="Slide 3"></div>
+    <!-- Add more slides as needed -->
+  </div>
+</div>
 </body>
 </html>
