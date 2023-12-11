80f5f893db73982354e75afefc6c4bf76306fe70
Mynul Islam <maynul.programmer24@gmail.com>
2023-12-11 21:52:02 +0600

    second

--- functions.php
+++ functions.php
@@ -1,8 +1,16 @@
 <?php
 /*
 *Mytheme Function
- */
+*/
 
 
- // This is Title
+// This is Title
  add_theme_support('title-tag');
+
+// Theme css and jQuery File calling
+
+function maynul_css_js_file_calling(){
+    wp_enqueue_style( 'maynul-style', get_stylesheet_uri()./css/bootstrap.css, array(),'5.3.2');
+}
+
+add_action('wp_enqueue_scripts','maynul_css_js_file_calling');
